28e285181081752a233927d4365adc2012333fea
IsacRFx <edwin.rodriguez193@tectijuana.edu.mx>
2023-10-19 01:42:17 -0700

    feat: load scripts - genres/movies/actors

--- movies/models.py
+++ movies/models.py
@@ -14,9 +14,17 @@ class Genre(models.Model):
 
 
 class Actor(models.Model):
+
+    GENDER_CHOICES = (
+        (0, "No especificado"),
+        (1, "Mujer"),
+        (2, "Hombre"),
+        (3, "No binario")
+    )
+
     tmdb_actor_id = models.IntegerField(unique=True)
     name = models.CharField(max_length=128)
-    sex = models.PositiveSmallIntegerField()
+    gender = models.PositiveSmallIntegerField(default=0, choices=GENDER_CHOICES)
     biography = models.TextField(blank=True)
     birthday = models.DateField(blank=True, null=True)
     place_of_birth = models.CharField(max_length=128, blank=True)
@@ -28,16 +36,16 @@ class Actor(models.Model):
 
 
 class Movie(models.Model):
+    tmdb_movie_id = models.IntegerField(blank=True, unique=True)
     title = models.CharField(max_length=200)
     overview = models.TextField()
-    release_date = models.DateTimeField()
+    release_date = models.DateField()
     budget = models.IntegerField(blank=True)
-    tmdb_movie_id = models.IntegerField(blank=True, unique=True)
+    running_time = models.IntegerField(default=0)
     revenue = models.IntegerField(blank=True)
     poster_path = models.URLField(blank=True)
     genres = models.ManyToManyField(Genre)
-    credits = models.ManyToManyField(Actor, through="MovieCredit")
-    # running_time = models.IntegerField() # this is not on tmdb api
+    credits = models.ManyToManyField(Actor, through="MovieCredit", related_name="credits")
 
     def __str__(self):
         return self.title
@@ -47,6 +55,9 @@ class MovieCredit(models.Model):
     actor = models.ForeignKey(Actor, on_delete=models.CASCADE)
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     role = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.actor.name + "->" + self.role
 
 
 class MovieReview(models.Model):
