33ef0944061877abc223bcd491daa2722e7e65ea
IsacRFx <edwin.rodriguez193@tectijuana.edu.mx>
2023-10-19 22:54:50 -0700

    test: initial catalog implementation

--- movies/views.py
+++ movies/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, redirect
 from .forms import SignUpForm
+from movies.models import Movie
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'movies/home.html')
+    movies = Movie.objects.all()
+    return render(request, 'movies/home.html', context={'movies': movies})
 
 def sign_up(request):
     if request.method == 'POST':
@@ -18,4 +20,4 @@ def sign_up(request):
     else:
         form = SignUpForm()
 
-    return render(request, 'registration/sign_up.html', {'form': form})
+    return render(request, 'registration/sign_up.html', context={'form': form})
